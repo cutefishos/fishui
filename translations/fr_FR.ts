6f74920e2ac276425ceb5ce641e4e247c11929bb
Roxfr <52124613+roxfr@users.noreply.github.com>
2021-11-22 22:37:01 +0100

    Create fr_FR.ts

---
+++ translations/fr_FR.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>TextField</name>
+    <message>
+        <location filename="../src/fish-style/TextField.qml" line="62"/>
+        <source>Cut</source>
+        <translation>Couper</translation>
+    </message>
+    <message>
+        <location filename="../src/fish-style/TextField.qml" line="68"/>
+        <source>Copy</source>
+        <translation>Copier</translation>
+    </message>
+    <message>
+        <location filename="../src/fish-style/TextField.qml" line="74"/>
+        <source>Paste</source>
+        <translation>Coller</translation>
+    </message>
+    <message>
+        <location filename="../src/fish-style/TextField.qml" line="79"/>
+        <source>Select All</source>
+        <translation>Tout sélectionner</translation>
+    </message>
+</context>
+</TS>
